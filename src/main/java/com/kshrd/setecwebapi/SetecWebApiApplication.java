e5ce538ca619a6a17d88bf33d93dcfd42d9b19fa
Sovannakk <sovannak.khengg@gmail.com>
2024-11-25 15:15:45 +0700

    add docker file and update docker compose

--- src/main/java/com/kshrd/setecwebapi/SetecWebApiApplication.java
+++ src/main/java/com/kshrd/setecwebapi/SetecWebApiApplication.java
@@ -1,9 +1,16 @@
 package com.kshrd.setecwebapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Web API", version = "1.0"), servers = @Server(url = "https://web-api.krorya.com"))
 public class SetecWebApiApplication {
 
     public static void main(String[] args) {
